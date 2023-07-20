66d40e97f9919dc6b10dd84597ecf274b8e2c579
psg980331@naver.com <psg980331@naver.com>
2023-07-20 11:09:38 +0900

    :sparkles: feat : Add Report ExceptionCode #S09P12A308

---
+++ backend/airlingo/src/main/java/com/ssafy/airlingo/global/entity/LanguageCode.java
@@ -0,0 +1,12 @@
+package com.ssafy.airlingo.global.entity;
+
+public enum LanguageCode {
+	KOR("KOR"),
+	ENG("ENG");
+
+	private String code;
+
+	LanguageCode(String code) {
+		this.code = code;
+	}
+}
---
+++ backend/airlingo/src/main/java/com/ssafy/airlingo/global/exception/IncorrectLanguageCodeException.java
@@ -0,0 +1,11 @@
+package com.ssafy.airlingo.global.exception;
+
+public class IncorrectLanguageCodeException extends RuntimeException{
+	public IncorrectLanguageCodeException() {
+		super(ExceptionCode.INCORRECT_LANGUAGE_CODE_EXCEPTION.getErrorMessage());
+	}
+
+	public IncorrectLanguageCodeException(String message) {
+		super(message);
+	}
+}
