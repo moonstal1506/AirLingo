0a1eb533b3c67e5953d18450086f084db3f012bd
psg980331@naver.com <psg980331@naver.com>
2023-07-21 17:30:46 +0900

    :white_check_mark: test : Test report user feat #S09P12A308-102

--- backend/airlingo/src/test/java/com/ssafy/airlingo/domain/report/service/ReportServiceImplTest.java
+++ backend/airlingo/src/test/java/com/ssafy/airlingo/domain/report/service/ReportServiceImplTest.java
@@ -7,6 +7,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.ssafy.airlingo.domain.report.dto.request.ReportUserRequestDto;
+import com.ssafy.airlingo.domain.user.entity.User;
+import com.ssafy.airlingo.domain.user.repository.UserRepository;
 import com.ssafy.airlingo.global.exception.IncorrectLanguageCodeException;
 
 @SpringBootTest
@@ -14,6 +18,9 @@ class ReportServiceImplTest {
 
 	@Autowired
 	private ReportServiceImpl reportService;
+
+	@Autowired
+	private UserRepository userRepository;
 
 	@Test
 	@DisplayName("신고항목을 조회할때 올바르지 않은 언어 코드를 사용하면 예외가 발생한다.")
@@ -34,12 +41,39 @@ class ReportServiceImplTest {
 
 	@Test
 	@DisplayName("유저 신고기능이 올바르게 동작하는지 테스트")
-	void 메서드명() {
+	void reportUserTest() {
 	    // given
+		ReportUserRequestDto report = ReportUserRequestDto.builder()
+			.userId(1L)
+			.reportItemId(1L)
+			.description("패드립 당했어요")
+			.build();
 
-	    // when
+		// when
+		Long reportId = reportService.reportUser(report);
 
-	    // then
+		// then
+		assertThat(reportId).isGreaterThan(0L);
+	}
+
+	@Test
+	@DisplayName("유저가 신고를 당하면 complain이 1증가 하여야 한다.")
+	void userComplaintCountTest() {
+		// given
+		ReportUserRequestDto report = ReportUserRequestDto.builder()
+			.userId(1L)
+			.reportItemId(1L)
+			.description("패드립 당했어요")
+			.build();
+
+		User user = userRepository.findById(1L).get();
+		int beforeUserComplainCount = user.getUserComplain();
+
+		// when
+		reportService.reportUser(report);
+
+		// then
+		assertThat(beforeUserComplainCount+1).isEqualTo(userRepository.findById(1L).get().getUserComplain());
 	}
 
 }
