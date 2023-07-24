d5f14f05fa8b81664bb4b8857a4391f76b1566a0
sujeong <msj1506@naver.com>
2023-07-24 16:09:37 +0900

    :sparkles: feat : Add RabbitMq #S09P12A308-113

--- backend/airlingo/src/main/java/com/ssafy/airlingo/domain/user/entity/User.java
+++ backend/airlingo/src/main/java/com/ssafy/airlingo/domain/user/entity/User.java
@@ -7,6 +7,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.ssafy.airlingo.domain.language.dto.request.LanguageDto;
 import com.ssafy.airlingo.domain.language.entity.Language;
 import com.ssafy.airlingo.domain.language.entity.UserLanguage;
+import com.ssafy.airlingo.domain.matching.request.MatchingRequestDto;
+import com.ssafy.airlingo.domain.matching.response.MatchingUserDto;
 import com.ssafy.airlingo.domain.user.dto.response.UserResponseDto;
 import com.ssafy.airlingo.global.entity.BaseTimeEntity;
 
@@ -116,5 +118,19 @@ public class User extends BaseTimeEntity {
 	public void removeUserLanguage(UserLanguage userLanguage) {
 		userLanguages.remove(userLanguage);
 		userLanguage.setUser(null);
+	}
+
+	public MatchingUserDto toMatchingUserDto(MatchingRequestDto matchingRequestDto) {
+		return MatchingUserDto.builder().
+			userNickname(this.userNickname)
+			.userImgUrl(this.userImgUrl)
+			.userNativeLanguage(this.userNativeLanguage.getLanguageName())
+			.userStudyLanguage(matchingRequestDto.getStudyLanguage())
+			.userInterestLanguages(this.userLanguages.stream()
+				.map(userLanguage -> userLanguage.getLanguage().getLanguageName())
+				.collect(Collectors.toList()))
+			.userRating(this.getUserRating())
+			.userBio(this.getUserBio())
+			.build();
 	}
 }
