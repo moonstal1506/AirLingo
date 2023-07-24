8fba6d9d6174e871306b735a7d793754fb47fa1a
suhwan2004 <suhwan2004@gmail.com>
2023-07-24 15:56:39 +0900

    :lipstick: design : Add global style, theme, font
    Co-authored-by: Crassula <Crassula1994@users.noreply.github.com>

---
+++ frontend/airlingo/src/assets/styles/GlobalStyle.js
@@ -0,0 +1,193 @@
+import { css } from "@emotion/react";
+
+const GlobalStyle = css`
+    @font-face {
+        font-family: "Pretendard";
+        font-weight: 100;
+        font-style: normal;
+        src: url("@/assets/fonts/Pretendard-Thin.otf") format("opentype");
+    }
+
+    @font-face {
+        font-family: "Pretendard";
+        font-weight: 200;
+        font-style: normal;
+        src: url("@/assets/fonts/Pretendard-Extralight.otf") format("opentype");
+    }
+
+    @font-face {
+        font-family: "Pretendard";
+        font-weight: 300;
+        font-style: normal;
+        src: url("@/assets/fonts/Pretendard-Light.otf") format("opentype");
+    }
+
+    @font-face {
+        font-family: "Pretendard";
+        font-weight: 400;
+        font-style: normal;
+        src: url("@/assets/fonts/Pretendard-Regular.otf") format("opentype");
+    }
+
+    @font-face {
+        font-family: "Pretendard";
+        font-weight: 500;
+        font-style: normal;
+        src: url("@/assets/fonts/Pretendard-Medium.otf") format("opentype");
+    }
+
+    @font-face {
+        font-family: "Pretendard";
+        font-weight: 600;
+        font-style: normal;
+        src: url("@/assets/fonts/Pretendard-Semibold.otf") format("opentype");
+    }
+
+    @font-face {
+        font-family: "Pretendard";
+        font-weight: 700;
+        font-style: normal;
+        src: url("@/assets/fonts/Pretendard-Bold.otf") format("opentype");
+    }
+
+    @font-face {
+        font-family: "Pretendard";
+        font-weight: 800;
+        font-style: normal;
+        src: url("@/assets/fonts/Pretendard-ExtraBold.otf") format("opentype");
+    }
+
+    @font-face {
+        font-family: "Pretendard";
+        font-weight: 900;
+        font-style: normal;
+        src: url("@/assets/fonts/Pretendard-Black.otf") format("opentype");
+    }
+    html,
+    body,
+    div,
+    span,
+    applet,
+    object,
+    iframe,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6,
+    p,
+    blockquote,
+    pre,
+    a,
+    abbr,
+    acronym,
+    address,
+    big,
+    cite,
+    code,
+    del,
+    dfn,
+    em,
+    img,
+    ins,
+    kbd,
+    q,
+    s,
+    samp,
+    small,
+    strike,
+    strong,
+    sub,
+    sup,
+    tt,
+    var,
+    b,
+    u,
+    i,
+    center,
+    dl,
+    dt,
+    dd,
+    ol,
+    ul,
+    li,
+    fieldset,
+    form,
+    label,
+    legend,
+    table,
+    caption,
+    tbody,
+    tfoot,
+    thead,
+    tr,
+    th,
+    td,
+    article,
+    aside,
+    canvas,
+    details,
+    embed,
+    figure,
+    figcaption,
+    footer,
+    header,
+    hgroup,
+    menu,
+    nav,
+    output,
+    ruby,
+    section,
+    summary,
+    time,
+    mark,
+    audio,
+    video {
+        margin: 0;
+        padding: 0;
+        border: 0;
+        font-size: 100%;
+        font: inherit;
+        vertical-align: baseline;
+    }
+    article,
+    aside,
+    details,
+    figcaption,
+    figure,
+    footer,
+    header,
+    hgroup,
+    menu,
+    nav,
+    section {
+        display: block;
+    }
+    body {
+        font-family: "Pretendard";
+        font-weight: 400;
+        line-height: 1;
+    }
+    ol,
+    ul {
+        list-style: none;
+    }
+    blockquote,
+    q {
+        quotes: none;
+    }
+    blockquote:before,
+    blockquote:after,
+    q:before,
+    q:after {
+        content: "";
+        content: none;
+    }
+    table {
+        border-collapse: collapse;
+        border-spacing: 0;
+    }
+`;
+
+export default GlobalStyle;
--- frontend/airlingo/src/main.jsx
+++ frontend/airlingo/src/main.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { ThemeProvider, Global } from "@emotion/react";
+import App from "@/App";
+import GlobalStyle from "@/assets/styles/GlobalStyle";
+import theme from "@/assets/styles/Theme";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <ThemeProvider theme={theme}>
+            <Global styles={GlobalStyle} />
+            <App />
+        </ThemeProvider>
     </React.StrictMode>,
 );
