3826c21c5c8eb51d39143e7caaf0c4f0e96c500d
suhwan2004 <suhwan2004@gmail.com>
2023-07-27 19:44:50 +0900

    :bug: fix : Delete test code

--- frontend/airlingo/src/App.jsx
+++ frontend/airlingo/src/App.jsx
@@ -1,28 +1,5 @@
-import styled from "@emotion/styled";
-import { ScriptSliderMenu } from "@/components/sliderMenu";
-import { ReactComponent as AIcon } from "@/assets/imgs/icons/dictionary-icon.svg";
-import { ReactComponent as BIcon } from "@/assets/imgs/icons/script-icon.svg";
-import { ReactComponent as CIcon } from "@/assets/imgs/icons/translator-icon.svg";
-
-const ScriptSliderContent = [
-    { Icon: AIcon, Content: () => <div>첫 번째 박스에요</div> },
-    { Icon: BIcon, Content: () => <div>두 번째 박스에요</div> },
-    { Icon: CIcon, Content: () => <div>세 번째 박스에요</div> },
-];
-
 function App() {
-    return (
-        <TestContainer>
-            <button type="button">123</button>
-            <ScriptSliderMenu contentGroup={ScriptSliderContent} />
-        </TestContainer>
-    );
+    return <div>App</div>;
 }
-
-const TestContainer = styled.div`
-    background-color: gray;
-    height: 100vh;
-    width: 100vw;
-`;
 
 export default App;
