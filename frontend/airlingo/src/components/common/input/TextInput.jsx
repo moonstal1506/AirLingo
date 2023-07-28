2125b4154a2b8c0e41c0ad6b1ee66d13b0682b20
suhwan2004 <suhwan2004@gmail.com>
2023-07-28 16:34:18 +0900

    :sparkles: feat : Create textinput component

--- frontend/airlingo/src/components/common/input/TextInput.jsx
+++ frontend/airlingo/src/components/common/input/TextInput.jsx
@@ -1,5 +1,48 @@
-function TextInput() {
-    return <div>TextInput</div>;
+import styled from "@emotion/styled";
+
+const radiusObj = {
+    small: "10px",
+    big: "20px",
+};
+
+function TextInput({
+    placeholder = "",
+    radius = "small",
+    width = "300px",
+    height = "50px",
+    maxLength = 50,
+}) {
+    return (
+        <TextInputWrapper
+            type="text"
+            placeholder={placeholder}
+            radius={radius}
+            width={width}
+            height={height}
+            maxLength={maxLength}
+        />
+    );
 }
+
+const TextInputWrapper = styled.input`
+    box-sizing: border-box;
+    border-radius: ${({ radius }) => radiusObj[radius]};
+    ${({ width, height }) => `
+        width: ${width};
+        height: ${height};
+    `};
+    border: 1px solid ${({ theme }) => theme.colors.primary4};
+    background-color: ${({ theme }) => theme.colors.primary1};
+    outline: none;
+    padding: 0 20px;
+    color: ${({ theme }) => theme.colors.secondary3};
+
+    &::placeholder {
+        color: ${({ theme }) => theme.colors.primary4};
+    }
+    &:hover {
+        cursor: pointer;
+    }
+`;
 
 export default TextInput;
