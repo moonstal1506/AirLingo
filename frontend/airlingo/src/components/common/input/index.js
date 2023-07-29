cf920f3957b49b27174092f0bc1b7509484def25
suhwan2004 <suhwan2004@gmail.com>
2023-07-29 15:54:14 +0900

    :sparkles: feat : Add mixiconbuttoninput import/export in input component folder

--- frontend/airlingo/src/components/common/input/index.js
+++ frontend/airlingo/src/components/common/input/index.js
@@ -1,5 +1,6 @@
 import CheckBox from "./CheckBox";
 import TextArea from "./TextArea";
 import TextInput from "./TextInput";
+import MixIconButtonInput from "./MixIconButtonInput";
 
-export { CheckBox, TextArea, TextInput };
+export { CheckBox, TextArea, TextInput, MixIconButtonInput };
