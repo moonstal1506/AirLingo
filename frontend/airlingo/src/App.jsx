c706ddad7402bb65bac602b7ad5440d064c11fb1
suhwan2004 <suhwan2004@gmail.com>
2023-08-01 00:22:48 +0900

    :sparkles: feat : Add routerprovider in app.jsx

--- frontend/airlingo/src/App.jsx
+++ frontend/airlingo/src/App.jsx
@@ -1,5 +1,18 @@
+import styled from "@emotion/styled";
+import { RouterProvider } from "react-router-dom";
+import routers from "./router";
+
 function App() {
-    return <div>App</div>;
+    return (
+        <MainContainer>
+            <RouterProvider router={routers} />
+        </MainContainer>
+    );
 }
+
+const MainContainer = styled.div`
+    width: 100%;
+    height: 100%;
+`;
 
 export default App;
