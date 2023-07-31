e2c214c57c6f997c7dbefc3ce379ca9ab39059d7
suhwan2004 <suhwan2004@gmail.com>
2023-08-01 00:23:58 +0900

    :bug: fix : edit getlogout response function naming

--- frontend/airlingo/src/api/auth.js
+++ frontend/airlingo/src/api/auth.js
@@ -1,11 +1,11 @@
 import instance from "./instance";
 import processApiResponse from "@/utils/api";
 
-const logout = async ({ responseFunc, data }) => {
+const getLogout = async ({ responseFunc, data }) => {
     const { userLoginId } = data;
     try {
         const response = await instance.get(`/logout/${userLoginId}`);
-        processApiResponse({ responseFunc, statusCode: response.statusCode });
+        processApiResponse({ responseFunc, response });
         return response;
     } catch (e) {
         // fix me! : 불순한 접근, 네트워킹 에러로 판단. e.response의 코드를 가지고 error 페이지로 이동하기!
@@ -13,4 +13,4 @@ const logout = async ({ responseFunc, data }) => {
     }
 };
 
-export default logout;
+export default getLogout;
