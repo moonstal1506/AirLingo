b5be417acdaa0cf07fd55cdcf7a24d3ce4eceda4
suhwan2004 <suhwan2004@gmail.com>
2023-08-01 12:13:15 +0900

    :sparkles: feat : Add auth logic in AuthLayout.jsx

--- frontend/airlingo/src/Layout/AuthLayout.jsx
+++ frontend/airlingo/src/Layout/AuthLayout.jsx
@@ -12,21 +12,42 @@ function AuthLayout({ children }) {
     const [userProfile, setUserProfile] = useState({});
     const { routeTo } = useRouter();
 
+    if (userLoginId <= 0) routeTo("/");
+
     const checkValidAuth = useCallback(async () => {
-        const { data } = await getUserProfile({
+        const apiRes = await getUserProfile({
             responseFunc: {
                 200: (response) => {
                     setUserProfile(response.data);
-                },
-                400: () => {
-                    dispatch(logoutUser());
-                    routeTo("/login");
                 },
             },
             data: { userLoginId },
         });
 
-        if (!data) {
+        if (!apiRes) {
+            await getLogout({
+                responseFunc: {
+                    200: () => {
+                        dispatch(logoutUser());
+                        routeTo("/login");
+                    },
+                },
+                data: { userLoginId: 123 },
+            });
+        }
+    }, [routeTo, userLoginId, dispatch]);
+
+    useEffect(() => {
+        /* 1차 체킹. 현재, 로컬에 지정된 상태로 user의 프로필 정보를 받아올 수 있는지 체크 */
+        async function fetchProfile() {
+            await checkValidAuth();
+        }
+        fetchProfile();
+    }, [children, checkValidAuth]);
+
+    /* 2차 체킹. 받아온 userProfile과 현재 store에 저장된 storeUser가 동일한지 체킹 */
+    Object.entries(storeUser).forEach(async ([key, value]) => {
+        if (Object.prototype.hasOwnProperty.call(userProfile, key) && userProfile[key] !== value) {
             await getLogout({
                 responseFunc: {
                     200: () => {
@@ -37,25 +58,9 @@ function AuthLayout({ children }) {
                 },
             });
         }
-    }, [routeTo, userLoginId, dispatch]);
-
-    useEffect(() => {
-        /* 1차 체킹. 현재, 로컬에 지정된 상태로 user의 프로필 정보를 받아올 수 있는지 체크 */
-        async function fetchProfile() {
-            checkValidAuth();
-        }
-        fetchProfile();
-    }, [children, checkValidAuth]);
-
-    /* 2차 체킹. 받아온 userProfile과 현재 store에 저장된 storeUser가 동일한지 체킹 */
-    Object.entries(storeUser).forEach(([key, value]) => {
-        if (Object.prototype.hasOwnProperty.call(userProfile, key) && userProfile[key] !== value) {
-            dispatch(logoutUser());
-            routeTo("/login");
-        }
     });
 
-    return { children };
+    return <>{children}</>;
 }
 
 AuthLayout.propTypes = {
