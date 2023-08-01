4b8effc12e1b7f701ea757578417ebb62e381ef3
suhwan2004 <suhwan2004@gmail.com>
2023-08-02 02:52:08 +0900

    :sparkles: feat : Create modal component

---
+++ frontend/airlingo/src/components/modal/Modal.jsx
@@ -0,0 +1,70 @@
+import { createPortal } from "react-dom";
+import PropTypes from "prop-types";
+import styled from "@emotion/styled";
+import Overlay from "../common/overlay";
+import iconConfig from "@/config";
+
+function Modal({ Icon, title, titleColor, iconColor, children }) {
+    return createPortal(
+        <Overlay>
+            <ModalContainer>
+                <ModalBox iconColor={iconColor}>
+                    <Icon />
+                    <ModalTitle titleColor={titleColor}>{title}</ModalTitle>
+                    {children}
+                </ModalBox>
+            </ModalContainer>
+        </Overlay>,
+        document.body.querySelector("#modal"),
+    );
+}
+
+Modal.propTypes = {
+    Icon: PropTypes.elementType.isRequired,
+    title: PropTypes.string.isRequired,
+    iconColor: PropTypes.string,
+    titleColor: PropTypes.string,
+    children: PropTypes.node.isRequired,
+};
+
+Modal.defaultProps = {
+    iconColor: "black",
+    titleColor: "black",
+};
+
+const ModalContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const ModalBox = styled.div`
+    display: flex;
+    width: 650px;
+    border-radius: 20px;
+    background: #fff;
+    box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.75);
+    padding: 30px 0px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 15px;
+
+    svg path {
+        fill: ${({ iconColor }) => iconConfig.color[iconColor]};
+    }
+`;
+
+const ModalTitle = styled.div`
+    color: ${({ titleColor }) => iconConfig.color[titleColor]};
+    text-align: center;
+    font-family: Pretendard;
+    font-size: 40px;
+    font-style: normal;
+    font-weight: 800;
+    line-height: 44px; /* 110% */
+`;
+
+export default Modal;
