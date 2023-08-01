588b25b28dd0adf30177d4e2e9c796688a419232
suhwan2004 <suhwan2004@gmail.com>
2023-08-02 02:53:10 +0900

    :sparkles: feat : Create get language request function

---
+++ frontend/airlingo/src/api/language.js
@@ -0,0 +1,15 @@
+import instance from "./instance";
+import processApiResponse from "@/utils/api";
+
+const getLanguage = async ({ responseFunc }) => {
+    try {
+        const response = await instance.get(`/language`);
+        processApiResponse({ responseFunc, response });
+        return response;
+    } catch (e) {
+        // fix me! : 불순한 접근, 네트워킹 에러로 판단. e.response의 코드를 가지고 error 페이지로 이동하기!
+        return e.response;
+    }
+};
+
+export default getLanguage;
