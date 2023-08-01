2e0ed16040d4e914fc5915422d823de595194d05
suhwan2004 <suhwan2004@gmail.com>
2023-08-01 12:07:33 +0900

    :sparkles: feat : add useEffect logic in App.jsx

--- frontend/airlingo/src/App.jsx
+++ frontend/airlingo/src/App.jsx
@@ -1,8 +1,14 @@
 import styled from "@emotion/styled";
 import { RouterProvider } from "react-router-dom";
+import { useEffect } from "react";
 import routers from "./router";
 
 function App() {
+    useEffect(() => {
+        window.onbeforeunload = function pushRefresh() {
+            window.scrollTo(0, 0);
+        };
+    }, []);
     return (
         <MainContainer>
             <RouterProvider router={routers} />
