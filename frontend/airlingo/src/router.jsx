fc780868453e2b70542cd2ab1895cdd6ccd8c896
suhwan2004 <suhwan2004@gmail.com>
2023-08-01 09:17:34 +0900

    :sparkles: feat : Add header exist props

--- frontend/airlingo/src/router.jsx
+++ frontend/airlingo/src/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Notfound from "./pages/Notfound";
 import AuthLayout from "./layout/AuthLayout";
+import Header from "./components/header";
 /* fix me! 페이지 추가에 따른 등록 필요! */
 const routerData = [
     {
@@ -10,6 +11,7 @@ const routerData = [
         label: "Home",
         element: <Home />,
         withAuth: false,
+        headerExist: true,
     },
     {
         id: 1,
@@ -17,6 +19,7 @@ const routerData = [
         label: "NotFound",
         element: <Notfound />,
         withAuth: false,
+        headerExist: true,
     },
 ];
 
@@ -25,12 +28,24 @@ const routers = createBrowserRouter(
         if (router.withAuth) {
             return {
                 path: router.path,
-                element: <AuthLayout>{router.element}</AuthLayout>,
+                element: (
+                    <AuthLayout>
+                        <div>
+                            {router.headerExist && <Header />}
+                            {router.element}
+                        </div>
+                    </AuthLayout>
+                ),
             };
         }
         return {
             path: router.path,
-            element: router.element,
+            element: (
+                <>
+                    {router.headerExist && <Header />}
+                    {router.element}
+                </>
+            ),
         };
     }),
 );
