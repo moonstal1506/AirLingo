20258da5a37e2bc037ce68dad35e842543b21496
ooyouj <ooyouj@naver.com>
2023-08-02 17:48:12 +0900

    :sparkles: feat : Add signupOk page feature #S09P12A308-309

--- frontend/airlingo/src/api/level.js
+++ frontend/airlingo/src/api/level.js
@@ -1,11 +1,10 @@
 import instance from "./instance";
 import processApiResponse from "@/utils/api";
 
-const getLanguage = async ({ responseFunc }) => {
+const getLevel = async ({ responseFunc }) => {
     try {
         const response = await instance.get(`/language`);
         processApiResponse({ responseFunc, response });
-        console.log("리스트 줄래?", response.data);
         return response;
     } catch (e) {
         // fix me! : 불순한 접근, 네트워킹 에러로 판단. e.response의 코드를 가지고 error 페이지로 이동하기!
@@ -13,4 +12,4 @@ const getLanguage = async ({ responseFunc }) => {
     }
 };
 
-export default getLanguage;
+export default getLevel;
