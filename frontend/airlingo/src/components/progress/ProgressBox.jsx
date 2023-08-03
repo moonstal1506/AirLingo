ea078c82f47e98ec2d159832cad8eca2fe190e6a
sujeong <msj1506@naver.com>
2023-08-03 11:08:52 +0900

    ✨ feat : Add ProgressBox proceeding state

--- frontend/airlingo/src/components/progress/ProgressBox.jsx
+++ frontend/airlingo/src/components/progress/ProgressBox.jsx
@@ -1,14 +1,18 @@
 import styled from "@emotion/styled";
 import PropTypes from "prop-types";
-import { ReactComponent as StepCircleIcon } from "@/assets/imgs/icons/step-circle-icon.svg";
+import theme from "@/assets/styles/Theme";
 
 // ----------------------------------------------------------------------------------------------------
 
-function ProgressBox({ text, step }) {
+const { gray } = theme.colors;
+
+// ----------------------------------------------------------------------------------------------------
+
+function ProgressBox({ text, step, isProceeding }) {
     return (
         <ProgressContainer>
-            <StepCircleIcon>{step}</StepCircleIcon>
-            <ProgressTextWrapper>{text}</ProgressTextWrapper>
+            <ProgressStepWrapper isProceeding={isProceeding}>{step}</ProgressStepWrapper>
+            <ProgressTextWrapper isProceeding={isProceeding}>{text}</ProgressTextWrapper>
         </ProgressContainer>
     );
 }
@@ -16,15 +20,33 @@ function ProgressBox({ text, step }) {
 ProgressBox.propTypes = {
     step: PropTypes.string.isRequired,
     text: PropTypes.string.isRequired,
+    isProceeding: PropTypes.bool.isRequired,
 };
 
 const ProgressContainer = styled.div`
     text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     gap: 8px;
 `;
 
+const ProgressStepWrapper = styled.div`
+    width: 30px; /* Adjust width as needed */
+    height: 30px; /* Adjust height as needed */
+    border-radius: 50%;
+    background-color: ${(props) => (props.isProceeding ? "#333" : gray)};
+    color: white;
+    font-size: 15px;
+    font-weight: bold;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+`;
+
 const ProgressTextWrapper = styled.div`
-    color: #333;
+    color: ${(props) => (props.isProceeding ? "#333" : gray)};
     font-size: 20px;
     font-weight: 400;
 `;
