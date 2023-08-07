ae80f47a093b6fe92bcfc16914584484a78d04ef
psg980331@naver.com <psg980331@naver.com>
2023-08-07 14:53:42 +0900

    :bug: fix : Modify url Script to script in index.js

--- frontend/airlingo/src/pages/mypage/index.js
+++ frontend/airlingo/src/pages/mypage/index.js
@@ -1,6 +1,6 @@
 import MyPageHome from "./MyPageHome";
 import StatisticHome from "./StatisticHome";
 import BasicInfoPage2 from "./BasicInfoPage2";
-import Script from "./Script";
+import Script from "./script";
 
 export { MyPageHome, BasicInfoPage2, Script, StatisticHome };
