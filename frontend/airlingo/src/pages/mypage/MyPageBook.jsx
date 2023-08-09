014cd0bcae4f04f8a1405ad82a113361cb088651
Haaarimmm <haaarimmm@gmail.com>
2023-08-09 17:54:33 +0900

    :sparkles: feat : Combine basicinfo page with statistichome #S09P12A308-406

--- frontend/airlingo/src/pages/mypage/MyPageBook.jsx
+++ frontend/airlingo/src/pages/mypage/MyPageBook.jsx
@@ -1,25 +1,34 @@
 import React, { useRef } from "react";
 import HTMLFlipBook from "react-pageflip";
 import styled from "@emotion/styled";
-import { MyPageHome, StatisticHome1, StatisticHome2 } from "@/pages/mypage";
+import {
+    MyPageHome,
+    BasicInfoPage1,
+    BasicInfoPage2,
+    StatisticHome1,
+    StatisticHome2,
+} from "@/pages/mypage";
 
 function MyPageBook() {
     const pageFlipRef = useRef(null);
 
-    const handlePageTurn = () => {
-        if (pageFlipRef.current) {
-            pageFlipRef.current.getPageFlip().flipNext();
-        }
-    };
+    // const handlePageTurn = () => {
+    //     if (pageFlipRef.current) {
+    //         pageFlipRef.current.getPageFlip().flipNext();
+    //     }
+    // };
 
     return (
         <MyPageContainer>
             <BookContainer>
                 <HTMLFlipBook ref={pageFlipRef} width={505} height={705}>
                     <div style={{ width: "100%", height: "100%" }} />
-                    <PageCover onClick={handlePageTurn} />
+                    {/* <PageCover onClick={handlePageTurn} /> */}
+                    <PageCover />
                     <Page1 />
                     <Page2 />
+                    <Page3 />
+                    <Page4 />
                 </HTMLFlipBook>
             </BookContainer>
         </MyPageContainer>
@@ -41,7 +50,7 @@ const BookContainer = styled.div`
 
 const PageCover = React.forwardRef((props, ref) => {
     return (
-        <div className="page page-cover" ref={ref} data-density="hard">
+        <div className="page" ref={ref} data-density="hard">
             <MyPageHome />
         </div>
     );
@@ -49,15 +58,31 @@ const PageCover = React.forwardRef((props, ref) => {
 
 const Page1 = React.forwardRef((props, ref) => {
     return (
-        <div className="page" ref={ref}>
-            <StatisticHome1 />
+        <div className="page" ref={ref} data-density="hard">
+            <BasicInfoPage1 />
         </div>
     );
 });
 
 const Page2 = React.forwardRef((props, ref) => {
     return (
-        <div className="page" ref={ref}>
+        <div className="page" ref={ref} data-density="hard">
+            <BasicInfoPage2 />
+        </div>
+    );
+});
+
+const Page3 = React.forwardRef((props, ref) => {
+    return (
+        <div className="page" ref={ref} data-density="hard">
+            <StatisticHome1 />
+        </div>
+    );
+});
+
+const Page4 = React.forwardRef((props, ref) => {
+    return (
+        <div className="page" ref={ref} data-density="hard">
             <StatisticHome2 />
         </div>
     );
