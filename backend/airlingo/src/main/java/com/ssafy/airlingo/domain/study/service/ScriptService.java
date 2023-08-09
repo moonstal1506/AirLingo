734eefa0801291191911c3452687453cfe06ae31
psg980331@naver.com <psg980331@naver.com>
2023-08-09 11:04:36 +0900

    :sparkles: feat : Modify recording login #S09P12A308-343

--- backend/airlingo/src/main/java/com/ssafy/airlingo/domain/study/service/ScriptService.java
+++ backend/airlingo/src/main/java/com/ssafy/airlingo/domain/study/service/ScriptService.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 public interface ScriptService {
 	ScriptResponseDto findScriptByScriptId(Long scriptId);
 	void deleteScriptById(Long scriptId);
-	ScriptAfterSTTResponseDto createScript(MultipartFile voiceFile , Long studyId , Long cardId) throws IOException, ParseException;
+	ScriptAfterSTTResponseDto createScript(String sessionId , Long studyId , Long cardId) throws IOException, ParseException;
 	void modifyScriptContent(ModifyScriptContentRequestDto modifyScriptContentRequestDto);
 	List<SentenceResponseDto> voiceFileSTT(String audioPath) throws ParseException;
 }
