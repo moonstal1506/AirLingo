0eeb41647248ec82bc8e8821af2aa80b98500fcf
suhwan2004 <suhwan2004@gmail.com>
2023-08-13 18:40:47 +0900

    :sparkles: Edit defualt / route

--- frontend/airlingo/src/router.jsx
+++ frontend/airlingo/src/router.jsx
@@ -1,6 +1,5 @@
+/* eslint-disable no-param-reassign */
 import { createBrowserRouter } from "react-router-dom";
-import Home from "./pages/home/Home";
-import Notfound from "./pages/Notfound";
 import AuthLayout from "./Layout/AuthLayout";
 import Header from "./components/header";
 import NotAuthLayout from "./Layout/NotAuthLayout";
@@ -9,32 +8,23 @@ import Login from "./pages/login/Login";
 import Script from "./pages/mypage/Script";
 import SignUp from "./pages/signup";
 import Meeting from "./pages/meeting";
-import { MatchHome, MatchQueue, MatchResult, MatchStandby } from "./pages/match";
+import { MatchQueue, MatchResult, MatchStandby } from "./pages/match";
 import { MyPageBook, BasicInfoHome, StatisticHome, StatisticGraph, ShopHome } from "./pages/mypage";
+import Error from "./pages/Error/Error";
+import App from "./App";
+import HomeLayout from "./Layout/HomeLayout";
 
 /* fix me! 페이지 추가에 따른 등록 필요! */
 const routerData = [
     {
         id: 0,
-        path: "/",
+        path: "",
         label: "Home",
-        element: <Home />,
-        withAuth: false,
         headerExist: true,
-        mustNotAuth: false,
     },
     {
         id: 1,
-        path: "/test",
-        label: "Test",
-        element: <Home />,
-        withAuth: false,
-        headerExist: true,
-        mustNotAuth: false,
-    },
-    {
-        id: 2,
-        path: "/signup",
+        path: "signup",
         label: "SignUp",
         element: <SignUp />,
         withAuth: false,
@@ -42,8 +32,8 @@ const routerData = [
         mustNotAuth: false,
     },
     {
-        id: 3,
-        path: "/login",
+        id: 2,
+        path: "login",
         label: "Login",
         element: <Login />,
         withAuth: false,
@@ -51,17 +41,8 @@ const routerData = [
         mustNotAuth: false,
     },
     {
-        id: 4,
-        path: "/matchhome",
-        label: "MatchHome",
-        element: <MatchHome />,
-        withAuth: true,
-        headerExist: true,
-        mustNotAuth: false,
-    },
-    {
-        id: 5,
-        path: "/matchqueue",
+        id: 3,
+        path: "matchqueue",
         label: "MatchQueue",
         element: <MatchQueue />,
         withAuth: false,
@@ -69,8 +50,8 @@ const routerData = [
         mustNotAuth: false,
     },
     {
-        id: 6,
-        path: "/matchresult",
+        id: 4,
+        path: "matchresult",
         label: "MatchResult",
         element: <MatchResult />,
         withAuth: false,
@@ -78,8 +59,8 @@ const routerData = [
         mustNotAuth: false,
     },
     {
-        id: 7,
-        path: "/matchstandby",
+        id: 5,
+        path: "matchstandby",
         label: "MatchStandby",
         element: <MatchStandby />,
         withAuth: false,
@@ -87,8 +68,8 @@ const routerData = [
         mustNotAuth: false,
     },
     {
-        id: 8,
-        path: "/meeting",
+        id: 6,
+        path: "meeting",
         label: "Meeting",
         element: <Meeting />,
         withAuth: true,
@@ -96,8 +77,8 @@ const routerData = [
         mustNotAuth: false,
     },
     {
-        id: 9,
-        path: "/mypage",
+        id: 7,
+        path: "mypage",
         label: "mypage",
         element: <MyPageBook />,
         withAuth: true,
@@ -105,8 +86,8 @@ const routerData = [
         mustNotAuth: false,
     },
     {
-        id: 10,
-        path: "/basicinfo",
+        id: 8,
+        path: "basicinfo",
         label: "basicinfo",
         element: <BasicInfoHome />,
         withAuth: true,
@@ -114,8 +95,8 @@ const routerData = [
         mustNotAuth: false,
     },
     {
-        id: 11,
-        path: "/statistic",
+        id: 9,
+        path: "statistic",
         label: "statistic",
         element: <StatisticHome />,
         withAuth: false,
@@ -123,8 +104,8 @@ const routerData = [
         mustNotAuth: false,
     },
     {
-        id: 12,
-        path: "/graph",
+        id: 10,
+        path: "graph",
         label: "graph",
         element: <StatisticGraph />,
         withAuth: true,
@@ -132,8 +113,8 @@ const routerData = [
         mustNotAuth: false,
     },
     {
-        id: 13,
-        path: "/script",
+        id: 11,
+        path: "script",
         label: "Script",
         element: <Script />,
         withAuth: true,
@@ -141,8 +122,8 @@ const routerData = [
         mustNotAuth: false,
     },
     {
-        id: 14,
-        path: "/wordbook",
+        id: 12,
+        path: "wordbook",
         label: "WordBook",
         element: <WordBook />,
         withAuth: true,
@@ -150,21 +131,12 @@ const routerData = [
         mustNotAuth: false,
     },
     {
-        id: 15,
-        path: "/shop",
+        id: 13,
+        path: "shop",
         label: "Shop",
         element: <ShopHome />,
         withAuth: true,
         headerExist: true,
-        mustNotAuth: false,
-    },
-    {
-        id: 15,
-        path: "*",
-        label: "NotFound",
-        element: <Notfound />,
-        withAuth: false,
-        headerExist: false,
         mustNotAuth: false,
     },
 ];
@@ -181,30 +153,38 @@ function seperatedHeaderCheckElement(router) {
     return router.element;
 }
 
-const routers = createBrowserRouter(
-    routerData.map((router) => {
-        // 1. 로그인 해야만 접근이 가능한 경우
-        if (router.withAuth) {
+const routers = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <Error />,
+        children: routerData.map((router) => {
+            if (router.path === "") {
+                router.element = <HomeLayout router={router} />;
+            }
+            // 1. 로그인 해야만 접근이 가능한 경우
+            if (router.withAuth) {
+                return {
+                    path: router.path,
+                    element: <AuthLayout>{seperatedHeaderCheckElement(router)}</AuthLayout>,
+                };
+            }
+
+            // 2. 로그인이 되지 않은 상태여야만 접근이 가능한 경우
+            if (router.mustNotAuth) {
+                return {
+                    path: router.path,
+                    element: <NotAuthLayout>{seperatedHeaderCheckElement(router)}</NotAuthLayout>,
+                };
+            }
+
+            // 3. 공통적으로 그냥 접근해도 되는 경우
             return {
                 path: router.path,
-                element: <AuthLayout>{seperatedHeaderCheckElement(router)}</AuthLayout>,
+                element: seperatedHeaderCheckElement(router),
             };
-        }
-
-        // 2. 로그인이 되지 않은 상태여야만 접근이 가능한 경우
-        if (router.mustNotAuth) {
-            return {
-                path: router.path,
-                element: <NotAuthLayout>{seperatedHeaderCheckElement(router)}</NotAuthLayout>,
-            };
-        }
-
-        // 3. 공통적으로 그냥 접근해도 되는 경우
-        return {
-            path: router.path,
-            element: seperatedHeaderCheckElement(router),
-        };
-    }),
-);
+        }),
+    },
+]);
 
 export default routers;
