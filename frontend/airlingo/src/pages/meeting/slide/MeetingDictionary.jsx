19825ae50bc8cf6e26051707c3c445c88be04da2
suhwan2004 <suhwan2004@gmail.com>
2023-08-15 22:50:21 +0900

    :bug: fix : Edit meeting dictionary button size

--- frontend/airlingo/src/pages/meeting/slide/MeetingDictionary.jsx
+++ frontend/airlingo/src/pages/meeting/slide/MeetingDictionary.jsx
@@ -162,8 +162,8 @@ function MeetingDictionary() {
                 </Container>
             </ItemBox>
             <ModalButtonBox>
-                <TextButton text="단어장 저장" width="200px" onClick={saveWord} />
-                <TextButton text="발음 듣기" width="200px" onClick={handleSpeak} />
+                <TextButton text="단어장 저장" width="130px" onClick={saveWord} />
+                <TextButton text="발음 듣기" width="130px" onClick={handleSpeak} />
             </ModalButtonBox>
         </DictionaryContainer>
     );
