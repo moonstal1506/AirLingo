cda391f33907090bee68c18b0b0e74f7816a3bb8
moonstal1506 <moonstal1506@gmail.com>
2023-08-15 23:31:23 +0900

    🐛 fix : Fix sentence list

--- backend/airlingo/src/main/java/com/ssafy/airlingo/domain/content/service/SentenceService.java
+++ backend/airlingo/src/main/java/com/ssafy/airlingo/domain/content/service/SentenceService.java
@@ -2,10 +2,8 @@ package com.ssafy.airlingo.domain.content.service;
 
 import java.util.List;
 
-import com.ssafy.airlingo.domain.content.dto.response.CardCodeResponseDto;
-import com.ssafy.airlingo.domain.content.dto.response.CardResponseDto;
 import com.ssafy.airlingo.domain.content.dto.response.SentenceResponseDto;
 
 public interface SentenceService {
-	SentenceResponseDto getSentence();
+	List<SentenceResponseDto> getSentence();
 }
