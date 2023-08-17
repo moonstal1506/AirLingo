778786c451cf61ff978b1a49676a4b7148c78bb1
suhwan2004 <suhwan2004@gmail.com>
2023-08-17 17:00:42 +0900

    :bug: fix : Rollback openvidu hook

--- frontend/airlingo/src/hooks/useOpenVidu.jsx
+++ frontend/airlingo/src/hooks/useOpenVidu.jsx
@@ -31,14 +31,12 @@ const useOpenVidu = () => {
             if (event.stream.typeOfVideo === "CAMERA") {
                 const subscriber = cameraSession.subscribe(event.stream, undefined);
                 setSubscribers((prevSubscribers) => [...prevSubscribers, subscriber]);
-                console.log(subscriber);
-                const response = await cameraSession.subscribeToSpeechToText(
+                await cameraSession.subscribeToSpeechToText(
                     event.stream,
                     languageCodeConfig.find(
                         (cur) => cur.languageId === otherUser.userStudyLanguageId,
                     ).languageCode,
                 );
-                console.log(response);
             }
         });
 
@@ -73,11 +71,8 @@ const useOpenVidu = () => {
         });
 
         cameraSession.on("exception", async (event) => {
-            console.log("shit : ", event.name);
-
             if (event.name === "SPEECH_TO_TEXT_DISCONNECTED") {
                 let speechToTextReconnected = false;
-                console.log("STT Disconnect!!!!");
                 while (!speechToTextReconnected) {
                     await new Promise((r) => {
                         setTimeout(r, 1000);
